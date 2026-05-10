1d5f7bdd1624117f33a17312c089f3303bddb559
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2026-05-10 23:45:24 +0800

    added url for cloudinary

--- backend/api/models/students.py
+++ backend/api/models/students.py
@@ -12,6 +12,9 @@ class Student(models.Model):
         on_delete=models.CASCADE,
         related_name='students'
     )
+    
+    def student_photo_path(instance, filename):
+        return f'student_photos/order_{instance.order_id}/{filename}'
 
     # Identity
     student_id = models.CharField(max_length=50)
@@ -21,13 +24,29 @@ class Student(models.Model):
 
     # Photos
     photo = models.ImageField(
-        upload_to='student_photos/',
-        null=True, blank=True
+        upload_to=student_photo_path,
+        null=True, blank=True,
     )
     processed_photo = models.ImageField(
         upload_to='processed_photos/',
         null=True, blank=True
     )
+    
+    #QR Code
+    qr_code_data = models.CharField(
+        max_length = 255,
+        unique = True,
+        null = True,
+        blank = True,
+        editable = False
+    )
+    
+    qr_code_url = models.URLField(
+        max_length=500,
+        null=True, blank=True,
+        help_text = 'Cloudinary URL for QR code'
+    )
+    
 
     # Status flags
     photo_status = models.CharField(
@@ -35,6 +54,8 @@ class Student(models.Model):
         choices=PhotoStatus.choices,
         default=PhotoStatus.PENDING
     )
+    
+
     is_approved = models.BooleanField(default=False)
     is_walk_in = models.BooleanField(default=False)
 
@@ -44,6 +65,9 @@ class Student(models.Model):
     class Meta:
         db_table = 'students'
         unique_together = ('order', 'student_id')
+        indexes = [
+            models.Index(fields=['qr_code_data'])
+        ]
 
     def __str__(self):
         return f"{self.student_id} - {self.full_name}"
