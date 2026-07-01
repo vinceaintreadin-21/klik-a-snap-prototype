3b9b7650d2ecaa2d416c4282e9f2b141795b110d
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2026-07-01 21:44:16 +0800

    added frontend url in origins

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -178,11 +178,13 @@ TEMPLATES = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://klik-a-snap-prototype.vercel.app/"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://klik-a-snap-prototype.vercel.app/"
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
