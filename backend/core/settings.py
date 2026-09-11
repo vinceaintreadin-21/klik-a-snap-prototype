d2032874541f2b17b6b19a02d14de7094444d0b0
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2026-09-11 17:56:34 +0800

    added use lts and host password

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -55,7 +55,8 @@ if not DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = os.getenv('EMAIL_HOST')
     EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-    EMAIL_USE_TLS = os.getenv('EMAIL_HOST_PASSWORD')
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@klik-a-snap.com')
 else:
     ALLOWED_HOSTS = ['*']
